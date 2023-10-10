053d25ae5bcc357d15601d5abd865953d527a247
DW <ldw0123@gmail.com>
2023-10-10 18:41:25 +0900

    study: node

--- node/math.js
+++ node/math.js
@@ -38,7 +38,6 @@ module.exports = {
 
 // ------- case 2, 3 -------
 // 파일에서 여러 개의 식별자를 객체 형태로 내보내는 경우.
-
 module.exports.add = (a, b) => a + b
 module.exports.minus = (a, b) => a - b
 module.exports.PI = 3.14
