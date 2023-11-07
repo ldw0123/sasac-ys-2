96661f241c17e2563aa3078fbbaf237e92bc1095
DW <ldw0123@gmail.com>
2023-11-07 13:33:04 +0900

    study: env

---
+++ node/node-backend/10-env/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+const dotenv = require('dotenv');
+// cross-env : 배포 버전이냐? 개발 버전이냐? 에 따라 다른 env 파일을 로드할 수 있게끔 도와준다
+
+// node.js 환경변수 확인
+// console.log(process.env);
+
+// index.js와 같은 위치에 있는 .env 파일을 불러와서 환경변수로 사용할 수 있게끔 한다
+dotenv.config({ path: path.join(__dirname, '') }); // path 직접 지정
+console.log('test var: ', process.env.TEST_VAR);
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log('Server Port: ', process.env.PORT);
+});
