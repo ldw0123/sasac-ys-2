d713876f099d096a4991bfa00f3ec20b260221bc
DW <ldw0123@gmail.com>
2023-11-07 13:43:54 +0900

    study: env

--- node/node-backend/10-env/index.js
+++ node/node-backend/10-env/index.js
@@ -9,7 +9,8 @@ const dotenv = require('dotenv');
 // console.log(process.env);
 
 // index.js와 같은 위치에 있는 .env 파일을 불러와서 환경변수로 사용할 수 있게끔 한다
-dotenv.config({ path: path.join(__dirname, '') }); // path 직접 지정
+dotenv.config({ path: path.join(__dirname, './config/envs/.env') }); // path(경로) 직접 지정
+
 console.log('test var: ', process.env.TEST_VAR);
 
 app.get('/', (req, res) => {
@@ -19,3 +20,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Server Port: ', process.env.PORT);
 });
+
+// package.json : Node.js 명령어 단축을 예약해 두는 곳
