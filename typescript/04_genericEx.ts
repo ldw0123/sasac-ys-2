fb8c8fe8a2a303df6bc3c096b240966e5aaad682
ldw0123 <ldw0123@gmail.com>
2023-12-14 18:46:22 +0900

    study: typescript

--- typescript/04_genericEx.ts
+++ typescript/04_genericEx.ts
@@ -17,8 +17,8 @@ console.log(arrElement<string>(['a'], 0)); // a
 // 첫번째 인자로 들어간 배열의 길이보다 큰 index 수(두번째 인자)가 전달된다면 return false 시키기!
 // console.log(arrElement<string>(['a'], 1)); // false
 function arrElement2<T>(arr: T[], index: number) {
-  if (arr.length < index) return false;
+  if (arr.length <= index) return false;
   return arr[index];
 }
 console.log(arrElement2<string>(['a', 'b', 'c'], 3)); // false
-console.log(arrElement<string>(['a'], 1));
+// console.log(arrElement<string>(['a'], 1));
--- typescript/05_react-with-ts/src/App.tsx
+++ typescript/05_react-with-ts/src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css';
 import Origin from './components/Origin';
+import PostList from './components/Practice/PostList';
 import TsComponent from './components/TsComponent';
 
 function App() {
   return (
     <div>
-      {/* 리액트 4번 실습 */}
+      {/* 공부 - 리액트 4번 실습 파일 */}
       <Origin />
       <hr />
       <TsComponent />
+      <hr />
+      {/* 실습 */}
+      <PostList />
     </div>
   );
 }
