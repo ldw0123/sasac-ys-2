141e2ddca4b634758049262caca0b8ee6d6e2c35
ldw0123 <ldw0123@gmail.com>
2024-01-23 18:34:50 +0900

    study: 클래스

--- java/java-sesac/src/_05_class/_01_class/Person.java
+++ java/java-sesac/src/_05_class/_01_class/Person.java
@@ -1,17 +1,22 @@
 package _05_class._01_class;
 
 // getter & setter
+// 멤버 변수에 접근 제어를 추가하고 데이터 캡슐화를 위해 사용된다
+// getter: 멤버 변수의 값을 반환하는 메서드. 일반적으로 get으로 시작
+// setter: 멤버 변수의 값을 설정하는 메서드. 일반적으로 set으로 시작. 매개 변수를 통해 전달된 값을 멤버 변수에 할당한다
 public class Person {
     // 필드 (private)
     private int age;
     private String name;
 
     // getter
+    // getName() 메서드는 이름 멤버 변수의 값을 반환
     public String getName() {
         return name;
     }
 
     // setter
+    // setName(String name) 메서드는 이름 멤버 변수에 전달된 값을 설정
     public void setName(String name) {
         this.name = name;
     }
