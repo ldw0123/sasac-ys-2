55f18be5b676b5c939cbd7d8aec229dbf9b95e5b
ldw0123 <ldw0123@gmail.com>
2024-02-15 14:02:06 +0900

    study: react-redux

--- react/08-redux/src/store/indexEx.js
+++
@@ -1,9 +0,0 @@
-// Redux 실습
-import { combineReducers } from 'redux';
-import bankReducerEx from './bankReducerEx';
-
-const rootReducer = combineReducers({
-  money: bankReducerEx,
-});
-
-export default rootReducer;
